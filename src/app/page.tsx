0b3d9c05c0bd8094f46c6c75b55cc229cf05d063
NguyenThoQuan <thoquan150903@gmail.com>
2024-06-22 09:24:37 +0700

    Custom Mantine

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import Link from "next/link";
 import { Button } from "@mantine/core";
 
 function Home() {
-  return (
-    <Button component={Link} href="/hello">
-      Next link button
-    </Button>
-  );
+  return <Button>Next link button</Button>;
 }
 
 export default Home;
