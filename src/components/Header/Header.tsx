5bb1e4ff125c909e9da9a6b2e0a236c82f5e7486
NguyenThoQuan <thoquan150903@gmail.com>
2024-06-24 09:16:01 +0700

    Update Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,63 @@
-import { Grid } from "@mantine/core";
+"use client";
+import { Autocomplete, Group, Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { IconSearch } from "@tabler/icons-react";
 import style from "./Header.module.scss";
-import Search from "./Search/Search";
+import Link from "next/link";
+
+const links = [
+  { link: "/about", label: "Features" },
+  { link: "/pricing", label: "Pricing" },
+  { link: "/learn", label: "Learn" },
+  { link: "/community", label: "Community" },
+];
 
 const Header = () => {
+  const [opened, { toggle }] = useDisclosure(false);
+
+  const items = links.map((link) => (
+    <Link
+      key={link.label}
+      href={link.link}
+      className={style.link}
+      onClick={(event) => event.preventDefault()}
+    >
+      {link.label}
+    </Link>
+  ));
+
   return (
-    <Grid p="15px 0px">
-      <Search />
-    </Grid>
+    <header className={style.header}>
+      <div className={style.inner}>
+        <Group>
+          <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+        </Group>
+
+        <Group>
+          <Autocomplete
+            className={style.search}
+            placeholder="Search"
+            leftSection={
+              <IconSearch width={"16px"} height={"16px"} stroke={1.5} />
+            }
+            data={[
+              "React",
+              "Angular",
+              "Vue",
+              "Next.js",
+              "Riot.js",
+              "Svelte",
+              "Blitz.js",
+            ]}
+            visibleFrom="xs"
+            radius={"xl"}
+          />
+          <Group ml={50} gap={5} className={style.links} visibleFrom="sm">
+            {items}
+          </Group>
+        </Group>
+      </div>
+    </header>
   );
 };
 
--- src/components/Header/Search/Search.tsx
+++ src/components/Header/Search/Search.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { Container, Flex, Input } from "@mantine/core";
+import { Container, Flex, Group, Autocomplete } from "@mantine/core";
 import style from "./Search.module.scss";
 import { IconSearch } from "@tabler/icons-react";
+import Link from "next/link";
 import Image from "next/image";
 
 const Search = () => {
@@ -9,13 +10,6 @@ const Search = () => {
     <Container className={style.search}>
       <Flex gap="10px" align="center">
         <Image src="/favicon-32x32.png" alt="logo" width={36} height={36} />
-        <Input
-          variant="filled"
-          placeholder="Tìm kiếm"
-          leftSection={<IconSearch size={18} />}
-          size={"md"}
-          radius={"xl"}
-        />
       </Flex>
     </Container>
   );
